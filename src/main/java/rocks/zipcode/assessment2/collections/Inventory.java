13172981376332f28d7d40a394ec59e7e89d42ea
santos <santoslopez890@gmail.com>
2023-07-24 13:30:47 -0400

    Completed Collections

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,8 +10,10 @@ public class Inventory {
     /**
      * @param strings list of strings to add / remove / fetch from
      */
-    public Inventory(List<String> strings) {
+    List <String> inventory=new ArrayList<>();
 
+    public Inventory(List<String> strings) {
+    this.inventory=strings;
     }
 
     /**
@@ -24,14 +27,14 @@ public class Inventory {
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        inventory.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        inventory.remove(item);
     }
 
     /**
@@ -39,6 +42,6 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        return inventory.size();
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,15 +1,26 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+    private HashMap<Integer,String> hashMap=new HashMap<>();
+
+
+
+
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
-    public void add(Integer monthNumber, String monthName) {
 
+    public void add(Integer monthNumber, String monthName) {
+        hashMap.put(monthNumber,monthName);
     }
 
     /**
@@ -17,7 +28,8 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+
+        return hashMap.get(monthNumber);
     }
 
     /**
@@ -25,7 +37,12 @@ public class MonthConversion {
      * @return - the ordinal of the month in the year
      */
     public int getNumber(String monthName) {
-        return (Integer)null;
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            if (Objects.equals(monthName, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return 0;
     }
 
     /**
@@ -33,7 +50,10 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        if((monthNumber > 0 && monthNumber <= 12)&&(hashMap.get(monthNumber)!=null)){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -41,14 +61,20 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        String [] months=new String []{"January", "February","March","April","May","June","July","August","September","October","November","December"};
+        for (int i = 0; i < months.length; i++) {
+            if(months[i].equals(monthName)){
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return hashMap.size();
     }
 
     /**
@@ -56,6 +82,6 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+        hashMap.put(monthNumber,monthName);
     }
 }
