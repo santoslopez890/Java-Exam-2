fccab9de85afa7f53b9809ea5bc964a2ebca6cff
santos <santoslopez890@gmail.com>
2023-07-24 11:54:03 -0400

    Nevermind now all the fundamentals are complete

--- src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/StringUtils.java
@@ -1,34 +1,48 @@
 package rocks.zipcode.assessment2.fundamentals;
 
+
+import java.util.regex.Pattern;
+
 /**
  * @author leon on 28/11/2018.
  */
 public class StringUtils {
+    private static Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
     /**
      * @param stringToBePadded - string value to be flushed right
-     * @param amountOfPadding - amount of padding to be flushed left
+     * @param amountOfPadding  - amount of padding to be flushed left
      * @return `stringToBePadded` flushed right by left-padding
      */
     public static String padLeft(String stringToBePadded, int amountOfPadding) {
-        return null;
+        char ch=' ';
+        String result=String.format("%"+amountOfPadding+"s",stringToBePadded).replace(' ',ch);
+        return result;
     }
 
     /**
      * @param stringToBePadded - string value to be flushed left
-     * @param amountOfPadding - amount of padding to be flushed right
+     * @param amountOfPadding  - amount of padding to be flushed right
      * @return `stringToBePadded` flushed right by right-padding
      */
     public static String padRight(String stringToBePadded, int amountOfPadding) {
-        return null;
+        char ch=' ';
+        String result=String.format("%" + (-amountOfPadding) + "s", stringToBePadded).replace(' ',ch);
+        return result;
     }
 
     /**
-     * @param stringToBeRepeated - string value to be repeated
+     * @param stringToBeRepeated   - string value to be repeated
      * @param numberOfTimeToRepeat - number of times to repeat `stringToBeRepeated`
      * @return the string repeated and concatenated `n` times
      */
     public static String repeatString(String stringToBeRepeated, int numberOfTimeToRepeat) {
-        return null;
+        String expected = "";
+        for (int i = 0; i < numberOfTimeToRepeat; i++) {
+            expected += String.valueOf(stringToBeRepeated);
+
+        }
+        return expected;
     }
 
     /**
@@ -36,7 +50,9 @@ public class StringUtils {
      * @return - true if string only contains alpha characters
      */
     public static Boolean isAlphaString(String string) {
-        return null;
+        string = string.replaceAll(" ", (""));
+        return string.matches("[a-zA-Z0-9]*");
+
     }
 
     /**
@@ -44,7 +60,10 @@ public class StringUtils {
      * @return - true if string only contains numeric characters
      */
     public static Boolean isNumericString(String string) {
-        return null;
+        if (string == null) {
+            return false;
+        }
+        return pattern.matcher(string).matches();
     }
 
     /**
@@ -52,6 +71,14 @@ public class StringUtils {
      * @return - true if string only contains special characters
      */
     public static Boolean isSpecialCharacterString(String string) {
-        return null;
+        for (int i = 0; i < string.length(); i++) {
+            if (!Character.isDigit(string.charAt(i))
+                    && !Character.isLetter(string.charAt(i))
+                    && !Character.isWhitespace(string.charAt(i))) {
+
+                return true;
+            }
+        }
+        return false;
     }
 }
